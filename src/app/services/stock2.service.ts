8bff43aa39939413bde0d7aa40c7e611c2d1597f
mf473 <maf915@126.com>
2018-06-06 00:31:41 +0800

    1

--- src/app/services/stock2.service.ts
+++ src/app/services/stock2.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { StockService, Stock } from './stock.service';
+import { LoggerService } from './logger.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Stock2Service implements StockService {
 
-  constructor() { }
+  constructor(public log:LoggerService) { }
   getStock(): Stock{
     return new Stock(2,'mafeng');
   }
